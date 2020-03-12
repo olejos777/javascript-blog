ddbed27bcb2ad322ab4044301f79f4aa93906ee7
Maciej Olejniczak <olejniczakk.maciejj@gmail.com>
2020-03-12 18:08:05 +0100

    update script.js with create and insert list of articles

--- js/script.js
+++ js/script.js
@@ -7,39 +7,38 @@
 
 const titleClickHandler = function(event) {
 
-  event.preventDefault(); //blokuje zmienianie się adresu po klikaniu w poszczególne linki
-  const clickedElement = this; //deklaracja stałej, która zwraca informację nt. klikniętego elementu (dzięki zastosowaniu 'this')
-  console.log('Link was clicked!'); //wyświetlenie komunikatu w console.log po kliknięciu w link
+  event.preventDefault();                                                   //blokuje zmienianie się adresu po klikaniu w poszczególne linki
+  const clickedElement = this;                                              //deklaracja stałej, która zwraca informację nt. klikniętego elementu (dzięki zastosowaniu 'this')
+  console.log('Link was clicked!');                                         //wyświetlenie komunikatu w console.log po kliknięciu w link
 
 
   /* [DONE] remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active'); //deklaracja stałej, która ma zwracać wszystkie określone selektory (w tym wypadku wszystkie aktywne linki wewnątrz div'a z klasą .titles)
-  for (let activeLink of activeLinks) { //wywołanie funkcji, która usuwa wszystkie aktywne linki poprzez usunięcie klasy 'active' (z listy zwróconej przez 'const activeLinks')
+  const activeLinks = document.querySelectorAll('.titles a.active');        //deklaracja stałej, która ma zwracać wszystkie określone selektory (w tym wypadku wszystkie aktywne linki wewnątrz div'a z klasą .titles)
+  for (let activeLink of activeLinks) {                                     //wywołanie funkcji, która usuwa wszystkie aktywne linki poprzez usunięcie klasy 'active' (z listy zwróconej przez 'const activeLinks')
     activeLink.classList.remove('active');
   }
 
   /* [DONE] add class 'active' to the clicked link */
   // console.log('clickedElement (with plus): ' + clickedElement);         //wyświetla adres strony + klasę/href klikniętego linka
-  clickedElement.classList.add('active') //wywołanie funkcji, która dodaje aktywny link porzez dodanie klasy 'active' do klikniętego elementu (dzięki zastosowaniu obiekutu 'this')
+  clickedElement.classList.add('active')                                   //wywołanie funkcji, która dodaje aktywny link porzez dodanie klasy 'active' do klikniętego elementu (dzięki zastosowaniu obiekutu 'this')
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post.active'); //deklaracja stałej, która ma zwracać wszystkie określone selektory (w tym wypadku wszystkie aktywne artykuły wewnątrz div'a z klasą .post)
+  const activeArticles = document.querySelectorAll('.post.active');        //deklaracja stałej, która ma zwracać wszystkie określone selektory (w tym wypadku wszystkie aktywne artykuły wewnątrz div'a z klasą .post)
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
   /* [DONE] get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute("href"); //deklaracja stałej, która ma zwracać wartość atrybutu #href dla klikniętego linka
-  //alert(articleSelector); wyświetla komunikat o treści 'elementAttribute'
+  const articleSelector = clickedElement.getAttribute("href");             //deklaracja stałej, która ma zwracać wartość atrybutu #href dla klikniętego linka
   console.log('#href of clicked element is:', articleSelector);
 
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-  const articleTarget = articleSelector.replace('#', ''); //deklaracja stałej, która zamienia ciąg znaków na inny zadeklarowany ciąg znaków (usunięcie '#' przed article)
+  const articleTarget = articleSelector.replace('#', '');                  //deklaracja stałej, która zamienia ciąg znaków na inny zadeklarowany ciąg znaków (usunięcie '#' przed article)
 
   /* [DONE] add class 'active' to the correct article */
-  document.getElementById(articleTarget).classList.add('active'); //wywołanie funkcji, która ustawi class='active' dla klikniętego elementu
+  document.getElementById(articleTarget).classList.add('active');          //wywołanie funkcji, która ustawi class='active' dla klikniętego elementu
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -57,12 +56,12 @@ const articleSelector = 'post',
 
 function generateTitleLinks() {
   /* [DONE] remove contents of titleList */
-  const titleList = document.querySelector(optTitleListSelector); //deklaracja stałej, która wybiera 'ul' z klasą 'titles'
-  titleList.innerHTML = ""; //wywołanie funkcji, która usuwa listę z taga 'ul' z klasą 'titles'
+  const titleList = document.querySelector(optTitleListSelector);           //deklaracja stałej, która wybiera 'ul' z klasą 'titles'
+  titleList.innerHTML = "";                                                 //wywołanie funkcji, która usuwa listę z taga 'ul' z klasą 'titles'
 
   /* [IN PROGRESS] for each article */
-  const articles = document.querySelectorAll(optArticleSelector); //deklaracja stałej, która wybiera wszystkie selectory spełniające warunek 'optArticleSelector'
-  for (let article of articles) { //wywołanie pętli, która zwraca informacje dla pojedyńczych artykułów z listy artykułów zwróconej przez 'articles'
+  const articles = document.querySelectorAll(optArticleSelector);           //deklaracja stałej, która wybiera wszystkie selectory spełniające warunek 'optArticleSelector'
+  for (let article of articles) {                                           //wywołanie pętli, która zwraca informacje dla pojedyńczych artykułów z listy artykułów zwróconej przez 'articles'
     //console.log('szczegóły artykułu: ', article);
 
     /* find the title element */
@@ -72,15 +71,20 @@ function generateTitleLinks() {
     /* get the article id */
     const articleId = article.getAttribute("id");
     console.log('Article ID to: ', articleId);
+    
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    //console.log (linkHTML);
+
+    /* insert link into titleList */
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+    console.log (titleList.innerHTML);
+
   }
-
-
-
-  /* get the title from the title element */
-
-  /* create HTML of the link */
-
-  /* insert link into titleList */
+  
+ 
+  
+  
 
 }
 
