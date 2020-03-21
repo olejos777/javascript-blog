fef2c6aa6d01b288491c89fac518915a28fe8da6
Maciej Olejniczak <olejniczakk.maciejj@gmail.com>
2020-03-21 18:04:20 +0100

    install ESLint, EditorConfig, StyleLint

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
     console.log('links:', links);
   });*/
 
-const titleClickHandler = function(event) {
+const titleClickHandler = function (event) {
 
   event.preventDefault();                                                   //blokuje zmienianie się adresu po klikaniu w poszczególne linki
   const clickedElement = this;                                              //deklaracja stałej, która zwraca informację nt. klikniętego elementu (dzięki zastosowaniu 'this')
@@ -20,7 +20,7 @@ const titleClickHandler = function(event) {
 
   /* [DONE] add class 'active' to the clicked link */
   // console.log('clickedElement (with plus): ' + clickedElement);         //wyświetla adres strony + klasę/href klikniętego linka
-  clickedElement.classList.add('active')                                   //wywołanie funkcji, która dodaje aktywny link porzez dodanie klasy 'active' do klikniętego elementu (dzięki zastosowaniu obiekutu 'this')
+  clickedElement.classList.add('active');                                  //wywołanie funkcji, która dodaje aktywny link porzez dodanie klasy 'active' do klikniętego elementu (dzięki zastosowaniu obiekutu 'this')
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
@@ -30,7 +30,7 @@ const titleClickHandler = function(event) {
   }
 
   /* [DONE] get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute("href");             //deklaracja stałej, która ma zwracać wartość atrybutu #href dla klikniętego linka
+  const articleSelector = clickedElement.getAttribute('href');             //deklaracja stałej, która ma zwracać wartość atrybutu #href dla klikniętego linka
   console.log('#href of clicked element is:', articleSelector);
 
 
@@ -39,16 +39,16 @@ const titleClickHandler = function(event) {
 
   /* [DONE] add class 'active' to the correct article */
   document.getElementById(articleTarget).classList.add('active');          //wywołanie funkcji, która ustawi class='active' dla klikniętego elementu
-}
+};
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
-  
+
 function generateTitleLinks() {
   /* [DONE] remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);           //deklaracja stałej, która zwraca 'ul' z klasą 'titles'
-  titleList.innerHTML = "";                                                 //wywołanie funkcji, która usuwa listę z taga 'ul' z klasą 'titles'
+  titleList.innerHTML = '';                                                 //wywołanie funkcji, która usuwa listę z taga 'ul' z klasą 'titles'
 
   let html = '';
 
@@ -56,17 +56,17 @@ function generateTitleLinks() {
   const articles = document.querySelectorAll(optArticleSelector);           //deklaracja stałej, która zwraca wszystkie selectory spełniające warunek 'optArticleSelector'
   for (let article of articles) {                                           //wywołanie pętli, która zwraca informacje dla pojedyńczych artykułów z listy artykułów zwróconej przez 'articles'
     //console.log('szczegóły artykułu: ', article);
-    
+
     /* [DONE] find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML; //deklaracja stałej, która zwraca wszystkie tytuły (string/innerHTML z tagów o klasie .'post-title')
     console.log('Tytuł artykułu: ', articleTitle);
 
     /* [DONE] get the article id */
-    const articleId = article.getAttribute("id");                           //deklaracja stałej, która zwraca wszystkie ID dla wszystkich tagów article (na podstawie 'cosnt articles')
+    const articleId = article.getAttribute('id');                           //deklaracja stałej, która zwraca wszystkie ID dla wszystkich tagów article (na podstawie 'cosnt articles')
     console.log('Article ID to: ', articleId);
-    
+
     /* [DONE] create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';     //deklaracja stałej, która zwraca ciąg znaków  
+    const linkHTML = '<li class="btn"><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';     //deklaracja stałej, która zwraca ciąg znaków  
     //console.log (linkHTML);
 
     /* [DONE] insert link into titleList */
@@ -74,21 +74,21 @@ function generateTitleLinks() {
     //console.log (titleList.innerHTML);
 
     //function insertHTML() {
-      //const putHTML = document.querySelector(optTitleListSelector);         //deklaracja stałej, która zwraca 'ul' z klasą 'titles'
-      //putHTML.insertAdjacentHTML("beforeend", linkHTML);                    //funkcja, która dodajekod HTML do wybranego elementu
-      //console.log (putHTML);
+    //const putHTML = document.querySelector(optTitleListSelector);         //deklaracja stałej, która zwraca 'ul' z klasą 'titles'
+    //putHTML.insertAdjacentHTML("beforeend", linkHTML);                    //funkcja, która dodajekod HTML do wybranego elementu
+    //console.log (putHTML);
     html = html + linkHTML;
     //console.log(html);
-    }
-    titleList.innerHTML = html;
-    
-    //insertHTML();
-  //}
-    
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
+  }
+  titleList.innerHTML = html;
 
-    for (let link of links) {
+  //insertHTML();
+  //}
+
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
+
+  for (let link of links) {
     link.addEventListener('click', titleClickHandler);
   }
 }
